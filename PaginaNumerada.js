d9d6d861f808e7ea9cf3a8cdfe0884ca594c21b0
CahDiasMusic <34837741+CahDiasMusic@users.noreply.github.com>
2017-12-27 18:21:53 -0200

    Update and rename paginanumerada.js to PaginaNumerada.js

--- PaginaNumerada.js
+++ PaginaNumerada.js
@@ -59,9 +59,7 @@ pageNavi.show=function(f,e,a){
 		c=1
 	}
 	
-	<script charset="UTF-8" />
-	
-	var b='<span class="pages">PÁGINAS'"</span>";
+	var b='<span class="pages">'PÁGINAS"</span> ";
 	
 	if(c>1){
 		b+='<a href="'+f[1]+'">'+pageNaviConf.firstText+"</a>"
